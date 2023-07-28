75998c4842a079645534b76629b77fd3b86514b0
Emon Chowdhury <emonchowdhury942858@gmail.com>
2023-07-28 15:04:31 +0600

    Todo-app Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ITodo } from './todo.interface';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Todo-App';
+
+  get intiTodo(): ITodo {
+    return {
+
+      id: null,
+      title: ''
+    }
+  }
+  searchELement: string = ''
+  todoLists: ITodo[] = [];
+  todo: ITodo = this.intiTodo;
+
+  //add data and also add data after edit
+  onAdd(): void {
+    if (this.todo.id) {
+      const findTodo = this.todoLists.find(item => item.id === this.todo.id)
+      if (findTodo) {
+        findTodo.title = this.todo.title
+      }
+    } else {
+      this.todo.id = Date.now()
+      this.todoLists.push({ ...this.todo })
+    }
+
+    this.todo = this.intiTodo;
+  }
+
+  //for edit data
+  onEditTodo(editTodo: ITodo) {
+    this.todo = { ...editTodo };
+  }
+
+  //for delete data
+  onDeleteTodo(id: number) {
+    this.todoLists = this.todoLists.filter(item => item.id !== id)
+  }
+
 }
